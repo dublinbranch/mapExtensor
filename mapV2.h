800032a8102ae4e1f924ff1e2fcd37e4a63444b1
Roy Bellingan <roy@tech.techadsmedia.com>
2021-01-30 18:10:00 +0000

    some love also for multimap

--- mapV2.h
+++ mapV2.h
@@ -32,3 +32,36 @@ class mapV2 : public std::map<K, V> {
 		}
 	}
 };
+
+
+template <typename K, typename V>
+class multiMapV2 : public std::multimap<K, V> {
+      public:
+	/*
+	 * Use like
+	map2<int, string> bla;
+	bla.insert({5, "ciao"});
+
+	if (auto f = bla.get(5)) {
+		cout << f.val << "\n";
+	} else {
+		cout << "nada\n";
+	}
+	 */
+	[[nodiscard]] auto get(const K& k) const {
+		struct Founded {
+			const V* val   = nullptr;
+			bool     found = false;
+			operator bool() const {
+				return found;
+			}
+		};
+
+		if (auto iter = this->find(k); iter != this->end()) {
+			return Founded{&iter->second, true};
+		} else {
+			return Founded();
+		}
+	}
+};
+
