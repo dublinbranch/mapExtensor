176126e599623899c429f307ac6b0798f2b58c3f
Roy Bellingan <roy@tech.techadsmedia.com>
2022-05-24 04:19:04 +0100

    1 header rimosso, 1 nota aggiunta

--- joinVector.h
+++ joinVector.h
@@ -1,10 +1,14 @@
 #ifndef JOINVECTOR_H
 #define JOINVECTOR_H
 
-#include <concepts>
 #include <string>
 #include <vector>
 
+/*
+use
+#include <fmt/ranges.h>
+fmt::print("{}\n", fmt::join(en2, " | "));
+*/
 std::string join(const auto& in, const char* sep = ",") {
     std::string out;
     for (const auto& el : in) {
