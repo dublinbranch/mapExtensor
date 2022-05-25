c43986ddd9f2f4859ca212c2aba6c78eff9c212d
Roy Bellingan <roy@tech.techadsmedia.com>
2022-05-26 00:46:40 +0100

    reformat and comment

--- joinVector.h
+++ joinVector.h
@@ -5,18 +5,18 @@
 #include <vector>
 
 /*
-use
+this function is nice as an helper. but in some cases you might want to use
 #include <fmt/ranges.h>
 fmt::print("{}\n", fmt::join(en2, " | "));
 */
 std::string join(const auto& in, const char* sep = ",") {
-    std::string out;
-    for (const auto& el : in) {
-        out += el;
-        out += sep;
-    }
-    out.pop_back();
-    return out;
+	std::string out;
+	for (const auto& el : in) {
+		out += el;
+		out += sep;
+	}
+	out.pop_back();
+	return out;
 }
 
 #endif // JOINVECTOR_H
